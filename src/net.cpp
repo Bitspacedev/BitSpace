64d85f8a6c540f10cf3cd7e3efe052f5d3b4245c
zero24x <zero@you-spam.com>
2018-04-28 23:55:27 +0200

    use only 1 syncnode with hostname

--- src/net.cpp
+++ src/net.cpp
@@ -1139,10 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-        {"185.144.156.228", "185.144.156.228"},
-        {"185.144.157.144", "185.144.157.144"},
-        {"185.144.157.212", "185.144.157.212"},
-        {"185.144.156.228", "185.144.156.228"},
+        {"bsx-syncnode-1.giize.com", "bsx-syncnode-1.giize.com"}
 };
 
 void ThreadDNSAddressSeed(void* parg)
